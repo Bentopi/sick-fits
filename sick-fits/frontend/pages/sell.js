be64b07153637a378465541f02ac3d8d9233e994
Brian Simmons <brian@travelboss.ai>
2018-10-29 06:40:29 -0600

    initial commit

---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const Sell = props => {
+  return (
+    <div>
+      <p>ijij!</p>
+      <Link href="/">
+        <a> Home!</a>
+      </Link>
+    </div>
+  );
+};
+
+export default Sell;
