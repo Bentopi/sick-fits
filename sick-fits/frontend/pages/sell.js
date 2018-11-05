1872dcec09615c837070a3b9ea4318a2f7d2b5d4
Brian Simmons <brian@travelboss.ai>
2018-11-05 08:06:26 -0700

    next js setup and some basic styles

--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,12 +1,7 @@
-import Link from "next/link";
-
 const Sell = props => {
   return (
     <div>
       <p>ijij!</p>
-      <Link href="/">
-        <a> Home!</a>
-      </Link>
     </div>
   );
 };
